55c825f24c29564f9acf4339f21ffdc0e3081fe1
Anton Baranov <abaranov@linuxfoundation.org>
2015-10-27 12:29:08 -0400

    Add cobbler::params class

---
+++ manifests/params.pp
@@ -0,0 +1,31 @@
+# == Class: cobbler::params
+#
+# Defines default values for variables inside whole cobbler class
+#
+# === Parameters
+#
+# None
+#
+# === Authors
+#
+# Anton Baranov <email:abaranov@linuxfoundation.org>
+class cobbler::params {
+  $ensure                 = 'present'
+  $package                = ['cobbler']
+  $package_ensure         = 'installed'
+  $service                = 'cobblerd'
+  $service_ensure         = 'running'
+  $service_enable         = true
+  $config_path            = '/etc/cobbler'
+  $config_file            = "${config_path}/settings"
+  $config_modules         = "${config_path}/modules.conf"
+  $cmd_cobbler            = '/usr/bin/cobbler'
+  # Default config you have just after cobbler is installed
+  # Just load yaml as cobbler use yaml format for $config_file
+  $default_cobbler_config = parseyaml(
+    file('cobbler/default_settings.yaml')
+  )
+  # Default configuration for cobbler modules
+  $default_modules_config = {
+  }
+}
