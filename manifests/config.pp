2a8d9ce2ea12053f74b7d931c7f6010361fdda60
cruelsmith <92088441+cruelsmith@users.noreply.github.com>
2023-08-14 15:39:52 +0200

    Update codebase to newer puppet version
    * Add stdlib 9.x compatibility
    * Update .gitignore

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,29 +1,18 @@
-# == Class cobbler::config
+# @summary Manages configuration files for cobbler
 #
-# Manages configuration files for cobbler
-#
-# === Parameters
-#
-# [*ensure*]
+# @param ensure
 #   The state of puppet resources within the module.
 #
-#   Type: String
-#   Values: 'present', 'absent'
-#   Default: 'present'
-#
-# [*cobbler_config*]
+# @param cobbler_config
 #   Hash of cobbler settings options. This hash is merged with the
 #   default_cobbler_config hash from params class. All this options go to the
 #   file defined with config_file parameter. There are no checks (yet?) done to
 #   verify options passed with that hash
 #
-#   Type: Hash
-#   Default: {}
-#
-# [*cobbler_modules_config*]
+# @param cobbler_modules_config
 #   Hash of cobbler modules configuration. This hash is
 #   merged with the default_modules_config hash from params class.
-#   Exapmple:
+#   Example:
 #     cobbler_modules_config => {
 #       section1            => {
 #         option1 => value1,
@@ -34,81 +23,58 @@
 #       }
 #     }
 #
-#   Type: Hash
-#   Default: {}
-#
-# [*config_path*]
+# @param config_path
 #   The absolute path where cobbler configuration files reside. This to prepend
 #   to config_file and config_modules options to build full paths to setttings
 #   and modules.conf files.
 #
-#   Type: String
-#   Default: /etc/cobbler
-#
-# [*config_file*]
+# @param config_file
 #   The title of main cobbler configuration file. The full path to that file is
 #   build by prepending config_file with config_path parameters
 #
-#   Type: String
-#   Default: settings
-#
-# [*config_modules*]
+# @param config_modules
 #   The title of cobbler modules configuration file. The full path to that file
 #   is build by prepending config_modules with config_path parameters
 #
-#   Type: String
-#   Default: modules.conf
-#
-# === Authors
-#
-# Anton Baranov <abaranov@linuxfoundation.org>
-class cobbler::config(
-  $ensure,
-  $cobbler_config,
-  $cobbler_modules_config,
-  $config_path,
-  $config_file,
-  $config_modules,
-){
-  # Validation
-  validate_absolute_path(
-    $config_path,
-  )
-  validate_hash(
-    $cobbler_config,
-    $cobbler_modules_config,
-  )
-  validate_re($ensure, ['^present$','^absent$'])
+# @author Anton Baranov <abaranov@linuxfoundation.org>
+class cobbler::config (
+  Enum[
+    'present',
+    'absent'
+  ] $ensure,
+  Hash $cobbler_config,
+  Hash $cobbler_modules_config,
+  Stdlib::Absolutepath $config_path,
+  String $config_file,
+  String $config_modules,
+) {
+  $_dir_ensure = $ensure ? {
+    'present' => directory,
+    default   => absent,
+  }
+  $_file_ensure = $ensure ? {
+    'present' => file,
+    default   => absent,
+  }
 
-  validate_string(
-    $config_file,
-    $config_modules
-  )
+  file { $config_path:
+    ensure => $_dir_ensure,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+  }
 
-
-  File {
+  file { "${config_path}/${config_file}":
+    ensure  => $_file_ensure,
+    content => template ('cobbler/yaml.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
   }
 
-  $_dir_ensure = $ensure ? {
-    'present' => 'directory',
-    default   => 'absent',
-  }
-  file {$config_path:
-    ensure => $_dir_ensure,
-  }
-  # Just convert to yaml
-  file {"${config_path}/${config_file}":
-    ensure  => $ensure,
-    content => template ('cobbler/yaml.erb')
-  }
-
-  cobbler::config::ini {'modules.conf':
+  cobbler::config::ini { 'modules.conf':
     ensure      => $ensure,
     config_file => "${config_path}/${config_modules}",
     options     => $cobbler_modules_config,
   }
-
 }
